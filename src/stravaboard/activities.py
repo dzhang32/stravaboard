e89e40d29eee5254060e395be09f44443924560c
David Zhang <dyzhang32@gmail.com>
2022-06-14 22:10:47 +0100

    feat: add MVP for obtaining activities

--- src/stravaboard/activities.py
+++ src/stravaboard/activities.py
@@ -1,5 +1,20 @@
-from .activities import StravaBase
+import requests
+from pandas.io.json import json_normalize
+
+from .base import StravaBase
 
 
-class Activites(StravaBase):
-    pass
+class Activities(StravaBase):
+
+    ACTIVITIES_URL = "https://www.strava.com/api/v3/athlete/activities"
+
+    def get_activities(self, n: str = 100):
+
+        header = {"Authorization": "Bearer " + self.access_token}
+        param = {"per_page": n, "page": 1}
+        activities = requests.get(
+            self.ACTIVITIES_URL, headers=header, params=param
+        ).json()
+        activities = json_normalize(activities)
+
+        self.activities = activities
