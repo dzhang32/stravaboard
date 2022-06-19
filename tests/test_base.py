0598906715910613e216295b328ce84c01e9779a
David Zhang <dyzhang32@gmail.com>
2022-06-19 09:51:00 +0100

    test(StravaBase): test update_access_token()

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,9 @@
 import os
+from datetime import datetime
 
 import pytest
 from dotenv import load_dotenv
+from tomlkit import datetime
 
 from stravaboard.base import StravaBase
 from stravaboard.exceptions import StravaRequestError
@@ -12,7 +14,7 @@ def load_env():
     load_dotenv()
 
 
-def test_StravaBase_retrieves_access_token_correctly():
+def test_StravaBase_retrieves_access_token_correctly(capfd):
     sb = StravaBase(
         client_id=os.environ.get("STRAVA_CLIENT_ID"),
         client_secret=os.environ.get("STRAVA_CLIENT_SECRET"),
@@ -21,6 +23,22 @@ def test_StravaBase_retrieves_access_token_correctly():
 
     assert len(sb.access_token) == 40
     assert isinstance(sb.access_token, str)
+    assert isinstance(sb.access_token_last_updated, datetime)
+    with pytest.raises(TypeError, match="must be of type datetime"):
+        sb.access_token_last_updated = "a_str"
+
+    # payload here should NOT be used, so set to arbitrary values
+    # if payload was used, would raise StravaRequestError
+    payload = {
+        "client_id": "x",
+        "client_secret": "y",
+        "refresh_token": "z",
+        "grant_type": "a",
+        "f": "b",
+    }
+    sb.update_access_token(payload)
+    out, _ = capfd.readouterr()
+    assert "no update needed." in out
 
 
 def test_StravaBase_errors_on_bad_credentials():
