994ba71dc3870bd84de074e4d76d57c236fc6478
David Zhang <dyzhang32@gmail.com>
2022-06-21 20:07:40 +0100

    build: simply management of dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="stravaboard",
+    version="0.1.0",
+    author="David Zhang",
+    author_email="dyzhang32@gmail.com",
+    description="A simple dashboard for displaying and analysing Strava data",
+    url="https://github.com/dzhang32/stravaboard",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
