17257ed6166d589bac7c97466e964d8f40db0eb8
David Zhang <dyzhang32@gmail.com>
2022-06-26 15:57:25 +0100

    0.3.0: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="stravaboard",
-    version="0.2.0",
+    version="0.3.0",
     author="David Zhang",
     author_email="dyzhang32@gmail.com",
     description="A simple dashboard for displaying and analysing Strava data",
