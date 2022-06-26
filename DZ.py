af068a05bc2a78c7511ebf394fec7fa0a37e7058
David Zhang <dyzhang32@gmail.com>
2022-06-26 15:48:55 +0100

    feat: use wrapper on streamlit pages

--- DZ.py
+++ DZ.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 import streamlit as st
-from stravaboard.streamlit import display_breakdown, display_summary, load_activities
+from stravaboard.streamlit import stravaboard
 
 st.set_page_config(page_title="Stravaboard")
 
@@ -17,27 +17,12 @@ load_dotenv()
 # load and tidy activity data
 now = datetime.now().strftime("%d/%m/%Y-%H")
 
-dz_act = load_activities(
+stravaboard(
+    name="David",
     datetime_now=now,
     client_id=os.environ.get("STRAVA_CLIENT_ID_DZ"),
     client_secret=os.environ.get("STRAVA_CLIENT_SECRET_DZ"),
     refresh_token=os.environ.get("STRAVA_REFRESH_TOKEN_DZ"),
-)
-
-st.title("David's Stravaboard 🏃‍♂️🏃‍♀️")
-
-display_summary(dz_act)
-
-st.header("The breakdown")
-
-display_breakdown(
-    dz_act, title="Date (x) vs distance (y), coloured by speed (white = fastest)"
-)
-
-# trigger streamlit refresh
-display_breakdown(
-    dz_act,
-    title="Date (x) vs speed (y), coloured by distance (blue = shortest)",
     y="speed_mins_per_km",
     color="distance_km",
     color_continuous_scale=["blue", "purple", "violet"],
--- pages/CG.py
+++ pages/CG.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 import streamlit as st
-from stravaboard.streamlit import display_breakdown, display_summary, load_activities
+from stravaboard.streamlit import stravaboard
 
 ##### Load environmental variables #####
 
@@ -17,26 +17,12 @@ st.set_page_config(page_title="Stravaboard")
 # load and tidy activity data
 now = datetime.now().strftime("%d/%m/%Y-%H")
 
-cg_act = load_activities(
+stravaboard(
+    name="Celestia",
     datetime_now=now,
     client_id=os.environ.get("STRAVA_CLIENT_ID_CG"),
     client_secret=os.environ.get("STRAVA_CLIENT_SECRET_CG"),
     refresh_token=os.environ.get("STRAVA_REFRESH_TOKEN_CG"),
-)
-
-st.title("Celestia's Stravaboard 🏃‍♂️🏃‍♀️")
-
-display_summary(cg_act)
-
-st.header("The breakdown")
-
-display_breakdown(
-    cg_act, title="Date (x) vs distance (y), coloured by speed (white = fastest)"
-)
-
-display_breakdown(
-    cg_act,
-    title="Date (x) vs speed (y), coloured by distance (white = shortest)",
     y="speed_mins_per_km",
     color="distance_km",
     color_continuous_scale=["blue", "purple", "violet"],
