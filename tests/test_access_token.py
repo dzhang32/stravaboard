68789bfcbad8a098e6575846c26991bd5cbd28de
David Zhang <32676710+dzhang32@users.noreply.github.com>
2022-09-10 22:30:14 +0100

    test: add tests for access token retrieval

---
+++ tests/test_access_token.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime
+
+import pytest
+from dotenv import load_dotenv
+
+from stravaboard.api.access_token import AccessTokenManager
+from stravaboard.exceptions import AccessTokenRequestError
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+def test_AccessTokenRequestError_retrieves_access_token_correctly():
+    atm = AccessTokenManager(
+        client_id=os.environ.get("STRAVA_CLIENT_ID_DZ"),
+        client_secret=os.environ.get("STRAVA_CLIENT_SECRET_DZ"),
+        refresh_token=os.environ.get("STRAVA_REFRESH_TOKEN_DZ"),
+    )
+
+    assert len(atm.access_token) == 40
+    assert isinstance(atm.access_token, str)
+    assert isinstance(atm.last_updated, datetime)
+
+
+def test_AccessTokenManager_errors_on_bad_credentials():
+    with pytest.raises(AccessTokenRequestError, match="Strava credentials"):
+        AccessTokenManager(
+            client_id="a",
+            client_secret="b",
+            refresh_token="c",
+        )
