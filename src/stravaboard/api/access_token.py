693ee1b62740cb6779211d95ea2e60cbbc1efa33
David Zhang <32676710+dzhang32@users.noreply.github.com>
2022-09-10 22:29:39 +0100

    feat: move access token responsibilities to class

--- src/stravaboard/api/access_token.py
+++ src/stravaboard/api/access_token.py
@@ -1,5 +1,40 @@
+from datetime import datetime
+
+import requests
+
+from stravaboard.exceptions import AccessTokenRequestError
+
+
 class AccessTokenManager:
-    def __init__(self, client_id: str, client_secret: str, refresh_token: str):
+    """Responsible for retrieving and storing the Strava access token."""
+
+    AUTH_URL = "https://www.strava.com/oauth/token"
+
+    def __init__(self, client_id: str, client_secret: str, refresh_token: str) -> None:
+        self.request_access_token(client_id, client_secret, refresh_token)
+
+    def request_access_token(
+        self, client_id: str, client_secret: str, refresh_token: str
+    ) -> None:
+        """Request a Strava access token.
+
+        Obtains an access token from Strava, which is required to send other
+        requests (e.g. to get activity data).
+
+        Parameters
+        ----------
+        client_id : str
+            Strava client ID.
+        client_secret : str
+            Strava client secret.
+        refresh_token : str
+            Strava refresh token.
+
+        Raises
+        ------
+        AccessTokenRequestError
+            Raised if the request fails (status code != 200).
+        """
         payload = {
             "client_id": client_id,
             "client_secret": client_secret,
@@ -8,4 +43,14 @@ class AccessTokenManager:
             "f": "json",
         }
 
-        self._request_access_token(payload)
+        res = requests.post(self.AUTH_URL, data=payload, verify=False)
+
+        if res.status_code != 200:
+            raise AccessTokenRequestError(
+                "Request denied, check your Strava credentials."
+            )
+
+        access_token = res.json()["access_token"]
+
+        self.access_token = access_token
+        self.last_updated = datetime.now()
