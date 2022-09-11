9b116d023f03389da0b37bdbb4218adae99674f3
David Zhang <32676710+dzhang32@users.noreply.github.com>
2022-09-11 19:38:23 +0100

    test: add tests for AcitivitiesManager

---
+++ tests/test_data_manager.py
@@ -0,0 +1,37 @@
+import os
+
+import pandas as pd
+import pytest
+from dotenv import load_dotenv
+
+from stravaboard.api.access_token import AccessTokenManager
+from stravaboard.api.data_manager import ActivitiesManager
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture
+def access_token():
+    atm = AccessTokenManager(
+        client_id=os.environ.get("STRAVA_CLIENT_ID_DZ"),
+        client_secret=os.environ.get("STRAVA_CLIENT_SECRET_DZ"),
+        refresh_token=os.environ.get("STRAVA_REFRESH_TOKEN_DZ"),
+    )
+
+    return atm.access_token
+
+
+def test_ActivitiesManager_retrieves_activities_correctly(access_token):
+
+    am = ActivitiesManager()
+
+    assert am.ACTIVITIES_URL == "https://www.strava.com/api/v3/athlete/activities"
+
+    am.get_data(access_token, n=50)
+    am.tidy_data()
+
+    assert isinstance(am.data, pd.DataFrame)
+    assert am.data.shape[0] == 50
