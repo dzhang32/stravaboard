70cb30eb3161f1303a4cd96dd2c47e94d4af8d16
David Zhang <32676710+dzhang32@users.noreply.github.com>
2022-09-13 23:28:55 +0100

    feat: add top-level class to query the strava api

--- src/stravaboard/api/strava_api.py
+++ src/stravaboard/api/strava_api.py
@@ -1,15 +1,36 @@
-import os
+import pandas as pd
 
 from stravaboard.api.access_token import AccessTokenManager
+from stravaboard.api.data_manager import DataManager
 
 
 class StravaAPI:
+    """Responsible for querying the Strava API."""
 
-    BASE_URL = "https://www.strava.com/"
-    AUTH_URL = os.path.join(BASE_URL, "oauth/token")
-
-    def __init__(self, client_id: str, client_secret: str, refresh_token: str):
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        refresh_token: str,
+        data_manager: DataManager,
+    ):
 
         self.access_token_manager = AccessTokenManager(
             client_id, client_secret, refresh_token
         )
+        self.data_manager = data_manager
+
+    def get(self) -> pd.DataFrame:
+        """Download and tidy Strava data.
+
+        Uses the DataManager to download and tidy the Strava data of interest.
+
+        Returns
+        -------
+        pd.DataFrame
+            _description_
+        """
+        self.data_manager.get_data(self.access_token_manager.access_token)
+        self.data_manager.tidy_data()
+
+        return self.data_manager.data
