f4440db003fbd064b8cabc342313f65841420851
David Zhang <32676710+dzhang32@users.noreply.github.com>
2022-09-19 13:21:20 +0100

    feat: change name displayed on browser

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 import os
 
+import streamlit as st
 from dotenv import load_dotenv
 
 from stravaboard.streamlit.components import Mileage, SpeedBreakdown, Summary
@@ -8,6 +9,8 @@ from stravaboard.streamlit.stravaboard import Stravaboard
 # load strava credentials from .env
 load_dotenv()
 
+# change the name of the page shown on browser
+st.set_page_config(page_title="Stravaboard")
 
 sb = Stravaboard(
     client_id=os.environ.get("STRAVA_CLIENT_ID_DZ"),
